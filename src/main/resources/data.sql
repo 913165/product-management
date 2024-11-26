aa92aab965d9ae7a51a9bcc3c82d3f1b40a813c0
Pritesh <tinumistry@gmail.com>
2024-11-26 23:53:57 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+-- Inserting a product with specific values
+INSERT INTO products (name, price, description) VALUES ('Product 1', 19.99, 'Description for Product 1');
+
+-- Inserting another product
+INSERT INTO products (name, price, description) VALUES ('Product 2', 29.99, 'Description for Product 2');
+
+-- Inserting a third product
+INSERT INTO products (name, price, description) VALUES ('Product 3', 39.99, 'Description for Product 3');
+
+-- Inserting a fourth product
+INSERT INTO products (name, price, description) VALUES ('Product 4', 49.99, 'Description for Product 4');
+
+-- Inserting a fifth product
+INSERT INTO products (name, price, description) VALUES ('Product 5', 59.99, 'Description for Product 5');
